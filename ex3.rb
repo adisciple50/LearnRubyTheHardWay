45944a160debfc9ffda22edc863992b6d15aab09
Jason Crockett <jesusislord3@gmx.co.uk>
2017-07-25 15:26:41 +0100

    exersize 3 floating point edition. finished studydrill 3.5

--- ex3.rb
+++ ex3.rb
@@ -2,25 +2,25 @@
 puts "I will now count my chickens:"
 
 #prints Hens and evaulates math between #{ and }.
-puts "Hens #{25 + 30 / 6}"
+puts "Hens #{25.0 + 30.0 / 6.0}"
 #prints Roosters and evaulates math between #{ and }.
-puts "Roosters #{100 - 25 * 3 % 4}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
 
 
 # prints contents of double quotes
 puts "I will now count my eggs:"
 # prints the outcome of the sum below , left of "puts".
-puts 3+2+21 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3.0 + 2.0 + 21.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 # prints contents of double quotes
-puts "Is it true that 3 + 2 < 5 - 7 ?"
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0 ?"
 # prints the outcome of the boolean evaluatation [is 3 + 2 'less than' 5 - 7?], left of "puts".
-puts 3 + 2< 5 - 7
+puts 3.0 + 2.0 < 5.0 - 7.0
 
 #prints Roosters and evaulates math between #{ and }.
-puts "What is 3 + 2? #{3 + 2}"
+puts "What is 3 + 2? #{3.0 + 2.0}"
 #prints Roosters and evaulates math between #{ and }.
-puts "What is 5 - 7? #{5 - 7}"
+puts "What is 5 - 7? #{5.0 - 7.0}"
 
 # ["put":"print"].
 # prints contents of double quotes
@@ -29,6 +29,6 @@ puts "oh, thats why it's false."
 puts "How about some more."
 
 # more evaluations stated according to the questions asked below.
-puts "Is it greater? #{5 > -2}"
-puts "Is it greater or equal? #{5>= -2}"
-puts "Is it less or equal? #{5 <= 2}"
+puts "Is it greater? #{5.0 > -2.0}"
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+puts "Is it less or equal? #{5.0 <= 2.0}"
