b256ee8d5b990583cb902b0673a713b5c1f9ebe5
Jason Crockett <jesusislord3@gmx.co.uk>
2017-07-25 14:45:42 +0100

    finished exercize 3. finished study drill 3.1, begining study drill 3.2

--- ex3.rb
+++ ex3.rb
@@ -1,20 +1,34 @@
+# prints contents of double quotes
 puts "I will now count my chickens:"
 
+#prints Hens and evaulates math between #{ and }.
 puts "Hens #{25 + 30 / 6}"
+#prints Roosters and evaulates math between #{ and }.
 puts "Roosters #{100 - 25 * 3 % 4}"
 
+
+# prints contents of double quotes
 puts "I will now count my eggs:"
+# prints the outcome of the sum below , left of "puts".
 puts 3+2+21 - 5 + 4 % 2 - 1 / 4 + 6
 
+# prints contents of double quotes
 puts "Is it true that 3 + 2 < 5 - 7 ?"
+# prints the outcome of the boolean evaluatation [is 3 + 2 'less than' 5 - 7?], left of "puts".
 puts 3 + 2< 5 - 7
 
+#prints Roosters and evaulates math between #{ and }.
 puts "What is 3 + 2? #{3 + 2}"
+#prints Roosters and evaulates math between #{ and }.
 puts "What is 5 - 7? #{5 - 7}"
 
+# ["put":"print"].
+# prints contents of double quotes
 puts "oh, thats why it's false."
+# prints contents of double quotes
 puts "How about some more."
 
+# more evaluations stated according to the questions asked below.
 puts "Is it greater? #{5 > -2}"
 puts "Is it greater or equal? #{5>= -2}"
 puts "Is it less or equal? #{5 <= 2}"
